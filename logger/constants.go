fe041fa37d172cbe160ea7a19fd2fda14dcffaf3
Lucas Mendes <lucasmends93@proton.me>
2025-10-06 01:51:31 -0300

    refactor(logger): update logging constants and remove unused log levels

--- logger/constants.go
+++ logger/constants.go
@@ -1,11 +1,17 @@
 package logger
 
+const minimumTickerMilliseconds = 300
+
+const defaultTickerMilliseconds = 500
+
+const defaultBufferSize = 15
+
+const defaultLogLevel = INFO
+
 type Level uint8
 
 const (
 	SILENT Level = iota
 	INFO
-	DEBUG
 	VERBOSE
-	ALL
 )
